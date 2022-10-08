c6833098bd64ccef61d4d3e36081580286bb341d
Danielle <dziemba88@gmail.com>
2022-10-07 23:50:19 -0600

    navbar and header

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import "./header.css"
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="hero"></div>
+        <div className="header-text">
+            <h1 className="header-title">Hometown Brew</h1>
+            <span className="header-subtitle">where locals meet their local brews</span>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import "./navbar.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import logo from "../../assets/logo-small.png"
+
+const Navbar = () => {
+  return (
+    <div className="main-nav">
+        <div className="button-logo">
+            {/* <div className="menu-nav-button nav-corner-box nav-corner-left">
+                <div className="tooltip-content">Menu</div>
+                <span className="bar bar1"></span>
+                <span className="bar bar2"></span>
+                <span className="bar bar3"></span>
+            </div> */}
+            <div className="logo">
+                <a itemProp="url" href="https://bookriot.com">
+                    <img src={logo} alt="Hometown Brew logo" />
+                </a>
+            </div>
+        </div>
+
+        <div className="nav-content-right">
+            <div className="search-button nav-corner-box nav-corner-right">
+                <FontAwesomeIcon icon={faMagnifyingGlass} className="header-icon" />
+            </div>
+        </div>
+
+        <div className="genre-nav">
+            <ul className="menu-masthead-1">
+                <li className="nav-newsletters menu-item">
+                    Login
+                </li>
+                <li className="nav-podcasts menu-item">
+                    Register
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
